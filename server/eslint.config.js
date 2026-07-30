7e9d7c8b990b7aebb4960a32953a70ee146eb7e8
KSHITIZ DIXIT <kshitizd171@gmail.com>
2026-07-30 12:03:22 +0530

    add: backend eslint config

---
+++ server/eslint.config.js
@@ -0,0 +1,21 @@
+import js from '@eslint/js';
+import globals from 'globals';
+
+export default [
+  { ignores: ['node_modules'] },
+  {
+    files: ['**/*.js'],
+    languageOptions: {
+      ecmaVersion: 2022,
+      globals: {
+        ...globals.node,
+        ...globals.jest,
+      },
+      sourceType: 'module',
+    },
+    rules: {
+      ...js.configs.recommended.rules,
+      'no-unused-vars': ['warn', { argsIgnorePattern: '^_' }],
+    },
+  },
+];
