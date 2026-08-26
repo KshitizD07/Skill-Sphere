6efab9fc9daaa2815a4cb16e873dfb3d2a1dca5e
KSHITIZ DIXIT <kshitizd171@gmail.com>
2026-08-26 20:17:31 +0530

    add:wrapper for all posts related features

---
+++ client/src/features/feed/feedAPI.js
@@ -0,0 +1,40 @@
+import BaseAPI from '../../services/BaseAPI';
+
+const unwrap = (res) => (res?.data !== undefined ? res.data : res);
+
+const FeedAPI = {
+  // --- Feed & Posts ---
+  getPosts: async (cursor = null, limit = 10) =>
+    BaseAPI.get(`/posts?limit=${limit}${cursor ? `&cursor=${cursor}` : ''}`),
+
+  getUserPosts: async (userId, cursor = null, limit = 10) =>
+    BaseAPI.get(`/posts/user/${userId}?limit=${limit}${cursor ? `&cursor=${cursor}` : ''}`),
+
+  createPost: async (data) => unwrap(await BaseAPI.post('/posts', data)),
+  updatePost: async (postId, content) => unwrap(await BaseAPI.put(`/posts/${postId}`, { content })),
+  deletePost: async (postId) => BaseAPI.delete(`/posts/${postId}`),
+
+  // --- Likes ---
+  likePost: async (postId) => BaseAPI.post(`/posts/${postId}/like`, {}),
+  getPostLikes: async (postId, cursor = null, limit = 20) =>
+    BaseAPI.get(`/posts/${postId}/likes?limit=${limit}${cursor ? `&cursor=${cursor}` : ''}`),
+
+  // --- Comments ---
+  getComments: async (postId, cursor = null, limit = 20) =>
+    BaseAPI.get(`/posts/${postId}/comments?limit=${limit}${cursor ? `&cursor=${cursor}` : ''}`),
+
+  createComment: async (postId, content, parentId = null) =>
+    unwrap(await BaseAPI.post(`/posts/${postId}/comments`, { content, parentId })),
+
+  likeComment: async (postId, commentId) =>
+    BaseAPI.post(`/posts/${postId}/comments/${commentId}/like`, {}),
+
+  deleteComment: async (postId, commentId) =>
+    BaseAPI.delete(`/posts/${postId}/comments/${commentId}`),
+
+  // --- Report ---
+  reportPost: async (postId, reason, detail = '') =>
+    BaseAPI.post(`/posts/${postId}/report`, { reason, detail }),
+};
+
+export default FeedAPI;
